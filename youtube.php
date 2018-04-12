7d659424b278b96273101d7e8c8c5aba48d9d979
Kushnerov Oleksandr <k-a-s2008@ukr.net>
2018-04-12 10:15:26 +0300

    Update youtube.php

--- youtube.php
+++ youtube.php
@@ -1,5 +1,6 @@
 <?php
 header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
 if (isset($_GET['url']) && isset($_GET['list'])) {
     $url = $_GET['url'];
     $list = $_GET['list'];
